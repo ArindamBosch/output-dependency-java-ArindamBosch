142a2d67d03c899ebe7cf56c0ba6a39545c95822
ArindamBosch <arindamdutta1970@gmail.com>
2025-07-31 09:55:59 +0530

    Update tshirts.java

--- tshirts/tshirts.java
+++ tshirts/tshirts.java
@@ -1,3 +1,5 @@
+
+// actual code
 public class Tshirts {
     static String size(int cms) {
         if(cms < 38) {
@@ -13,21 +15,7 @@ public class Tshirts {
         assert(size(37) == "S");
         assert(size(40) == "M");
         assert(size(43) == "L");
+        assert(size(38) == "XM");        // not there added 
         System.out.println("All is well (maybe!)");
-    }
-}
-
-
-
-public class TshirtsTest {
-    public static void main(String[] args) {
-        assert(Tshirts.size(37).equals("S"));
-        assert(Tshirts.size(40).equals("M"));
-        assert(Tshirts.size(43).equals("L"));
-
-        // 🔥 This test will fail due to the bug
-        assert(Tshirts.size(38).equals("M")) : "Expected size(38) to be M, but got: " + Tshirts.size(38);
-
-        System.out.println("Test finished (should fail!)");
     }
 }
