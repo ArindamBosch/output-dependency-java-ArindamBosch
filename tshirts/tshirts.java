2edf48e6e9a6c82aa7564bf5b6744dbc0bcf3f88
ArindamBosch <arindamdutta1970@gmail.com>
2025-07-31 09:12:47 +0530

    Update tshirts.java
    Tshirt TC

--- tshirts/tshirts.java
+++ tshirts/tshirts.java
@@ -16,3 +16,18 @@ public class Tshirts {
         System.out.println("All is well (maybe!)");
     }
 }
+
+
+
+public class TshirtsTest {
+    public static void main(String[] args) {
+        assert(Tshirts.size(37).equals("S"));
+        assert(Tshirts.size(40).equals("M"));
+        assert(Tshirts.size(43).equals("L"));
+
+        // 🔥 This test will fail due to the bug
+        assert(Tshirts.size(38).equals("M")) : "Expected size(38) to be M, but got: " + Tshirts.size(38);
+
+        System.out.println("Test finished (should fail!)");
+    }
+}
