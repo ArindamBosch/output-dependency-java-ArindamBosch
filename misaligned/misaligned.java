52b433839d8060d233060a9b2135acd12e5f5998
ArindamBosch <arindamdutta1970@gmail.com>
2025-07-31 09:10:05 +0530

    Update misaligned.java
    misaligned TC

--- misaligned/misaligned.java
+++ misaligned/misaligned.java
@@ -1,16 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Misaligned {
-    static int printColorMap() {
-        String majorColors[] = {"White", "Red", "Black", "Yellow", "Violet"};
-        String minorColors[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
-        int i = 0, j = 0;
-        for(i = 0; i < 5; i++) {
-            for(j = 0; j < 5; j++) {
-                System.out.printf("%d | %s | %s\n", i * 5 + j, majorColors[i], minorColors[i]);
+    static String[] majorColors = {"White", "Red", "Black", "Yellow", "Violet"};
+    static String[] minorColors = {"Blue", "Orange", "Green", "Brown", "Slate"};
+
+    static List<String> getColorMapLines() {
+        List<String> output = new ArrayList<>();
+        for (int i = 0; i < majorColors.length; i++) {
+            for (int j = 0; j < minorColors.length; j++) {
+                String line = String.format("%d | %s | %s", i * 5 + j, majorColors[i], minorColors[i]);
+                output.add(line);
             }
         }
-        return i * j;
+        return output;
     }
-    public static void main(String[] args) { 
+
+    static int printColorMap() {
+        List<String> lines = getColorMapLines();
+        for (String line : lines) {
+            System.out.println(line);
+        }
+        return lines.size();
+    }
+
+    public static void main(String[] args) {
         int result = printColorMap();
         assert(result == 25);
         System.out.println("All is well (maybe!)");
